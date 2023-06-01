9aece131e0146625b7e18ba85cd4fedb84b762b2
saum pandey <saumpandey@gmail.com>
2023-06-01 21:16:12 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,10 @@
+public class practice{
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        System.out.println("susgys");
+
+    }
+    public static int addition(int a , int b){
+        return a+b;
+    }
+}
